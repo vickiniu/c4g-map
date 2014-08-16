3f36d27be3f4ec2021beb1ba8ba5ca9fc3d8cc91
vickiniu <vicki.niu@gmail.com>
2014-08-15 19:14:02 -0700

    first commit

---
+++ javascript/scripts/color-map.js
@@ -0,0 +1,9 @@
+colors[] = {};
+
+colors['gh'] = colors['HN'] = colors['KE'] = colors['LR'] = colors['ML'] = colors['PK'] = colors['PA'] = colors['PH'] = colors['SN'] = 
+  colors['SL'] = colors['TZ'] = colors['ZM'] = colors['ZW'] = '#000000';
+
+$('#world-map').vectorMap({
+  colors: colors
+}
+);
