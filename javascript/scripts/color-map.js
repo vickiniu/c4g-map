4628842a2b777f7229e47c4f5023e12cb6efa85a
vickiniu <vicki.niu@gmail.com>
2014-08-25 12:21:26 -0700

    adding WCE footer

--- javascript/scripts/color-map.js
+++
@@ -1,9 +0,0 @@
-colors[] = {};
-
-colors['gh'] = colors['HN'] = colors['KE'] = colors['LR'] = colors['ML'] = colors['PK'] = colors['PA'] = colors['PH'] = colors['SN'] = 
-  colors['SL'] = colors['TZ'] = colors['ZM'] = colors['ZW'] = '#000000';
-
-$('#world-map').vectorMap({
-  colors: colors
-}
-);
